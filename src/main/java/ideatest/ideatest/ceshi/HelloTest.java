b92c80dc9f5ddfe0e07777a96fb466d38905baa6
duan <duanhaifeng@deppon.com>
2020-09-12 11:18:37 +0800

    dqdwq\

--- src/main/java/ideatest/ideatest/ceshi/HelloTest.java
+++ src/main/java/ideatest/ideatest/ceshi/HelloTest.java
@@ -2,10 +2,7 @@ package ideatest.ideatest.ceshi;
 
 public class HelloTest {
     public static void main(String [] args){
-        System.out.println("浣犲ソ");
-        System.out.println("1111");
-        System.out.println("2222");
-        System.out.println("23121231");
+      
         System.out.println("444");
         System.out.println("我是仓储第一帅饶金良");
         System.out.println("我饶金良");
--- src/main/java/ideatest/ideatest/ceshi/Test11.java
+++ src/main/java/ideatest/ideatest/ceshi/Test11.java
@@ -4,6 +4,6 @@ public class Test11 {
 
 	public static void main(String[] args) {
 		System.out.println("nishibushi1you");
-		System.out.println("2222");
+		System.out.println("nishibushi");
 	}
 }
