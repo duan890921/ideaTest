49237436da0d3bed957addde038e7f9a733745cf
duan <709784872@qq.com>
2020-09-12 11:22:05 +0800

    这是第一波3

--- src/main/java/ideatest/ideatest/ceshi/HelloTest.java
+++ src/main/java/ideatest/ideatest/ceshi/HelloTest.java
@@ -4,7 +4,7 @@ public class HelloTest {
     public static void main(String [] args){
       
         System.out.println("444");
-        System.out.println("我是仓储第一帅饶金良");
+        System.out.println("我是仓储第一帅饶金良1");
         System.out.println("我饶金良");
         System.out.println("我是");
     }
