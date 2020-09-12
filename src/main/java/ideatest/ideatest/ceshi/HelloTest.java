f91cff5c452f41ddc0e70b1c9f4f0d42fcc534f3
duan <duanhaifeng@deppon.com>
2020-09-12 10:42:54 +0800

    chongti2

--- src/main/java/ideatest/ideatest/ceshi/HelloTest.java
+++ src/main/java/ideatest/ideatest/ceshi/HelloTest.java
@@ -4,6 +4,7 @@ public class HelloTest {
     public static void main(String [] args){
         System.out.println("浣犲ソ");
         System.out.println("1111");
+        System.out.println("2222");
         
     }
 }
