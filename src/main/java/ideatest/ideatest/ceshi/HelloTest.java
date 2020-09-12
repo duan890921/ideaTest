02a2941da13604436068dce6cd8c4bdb1152cc60
duan <duanhaifeng@deppon.com>
2020-09-12 10:28:35 +0800

    制造冲突

--- src/main/java/ideatest/ideatest/ceshi/HelloTest.java
+++ src/main/java/ideatest/ideatest/ceshi/HelloTest.java
@@ -2,6 +2,8 @@ package ideatest.ideatest.ceshi;
 
 public class HelloTest {
     public static void main(String [] args){
-        System.out.println("你好");
+        System.out.println("浣犲ソ");
+        System.out.println("1111");
+        
     }
 }
