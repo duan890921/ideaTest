b5952bf1fd4619a34c98009063a084534289b04d
duan <709784872@qq.com>
2020-09-24 08:52:17 +0800

    这是第一波5

--- src/main/java/ideatest/ideatest/ceshi/Test11.java
+++ src/main/java/ideatest/ideatest/ceshi/Test11.java
@@ -3,6 +3,7 @@ package ideatest.ideatest.ceshi;
 public class Test11 {
 
 	public static void main(String[] args) {
+		String s="";
 		System.out.println("nishibushi1you");
 		System.out.println("1231231");
 	}
