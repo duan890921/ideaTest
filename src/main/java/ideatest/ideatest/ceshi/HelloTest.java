f3bc1aee3c83df76b690982700bc4aa4085f3a32
duan <duanhaifeng@deppon.com>
2020-09-24 08:57:33 +0800

    21·312

--- src/main/java/ideatest/ideatest/ceshi/HelloTest.java
+++ src/main/java/ideatest/ideatest/ceshi/HelloTest.java
@@ -3,8 +3,8 @@ package ideatest.ideatest.ceshi;
 public class HelloTest {
     public static void main(String [] args){
       
-        System.out.println("444555");
+        System.out.println("你好");
         System.out.println("我是仓储第一帅饶金良1");
-        System.out.println("6666112");
+        System.out.println("你呢");
     }
 }
